1a7dc07d8bf9102e9bd3697900b77f619e3a30f3
Alexys Groom <lexygroom@gmail.com>
2023-05-27 02:11:23 -0400

    file creation

---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,12 @@
+const finalPosition = function (moves) {
+  // Your code in here ...
+}
+
+//If we called the function with the following arguments.
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+finalPosition(moves);
+
+//The finalPosition function should return the array:
+//[-1, 4]
