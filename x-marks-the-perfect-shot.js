9536eff996acaf50fbb4dd6a607e199bf207e4e0
Alexys Groom <lexygroom@gmail.com>
2023-06-13 18:05:56 -0400

    Using an array to navigate an xy grid

--- x-marks-the-perfect-shot.js
+++ x-marks-the-perfect-shot.js
@@ -1,12 +1,33 @@
 const finalPosition = function (moves) {
-  // Your code in here ...
+  let x = 0
+  let y = 0
+
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === 'north') {
+      y++;
+    } else if (moves[i] === 'south') {
+      y--;
+    } else if (moves[i] === 'west') {
+      x--;
+    } else if (moves[i] === 'east') {
+      x++; 
+    }
+  }
+  return [x, y];
 }
+
+/*
+north = y+1
+south = y-1
+west = x-1
+east = x+1
+*/
 
 //If we called the function with the following arguments.
 
-const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']
 
-finalPosition(moves);
+console.log(finalPosition(moves));
 
 //The finalPosition function should return the array:
 //[-1, 4]
