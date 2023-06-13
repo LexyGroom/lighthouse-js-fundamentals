07e060456b150ac4bf76ccff885d553e2d2348fa
Alexys Groom <lexygroom@gmail.com>
2023-06-13 18:37:24 -0400

    Creating a function to determine age

--- functions-age-calculator.js
+++ functions-age-calculator.js
@@ -1,26 +1,10 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
 
-Create a function to determine someone's age.
+let age = currentYear - yearOfBirth;
 
-Define a function called ageCalculator. This function should take 3 parameters:
+return name + " is " + age + " years old.";
+}
 
-name - a string representing someones name
-yearOfBirth - a number representing their year of birth
-currentYear - a number representing the current year
-The ageCalculator function should return a string explaining how old the person is. 
-For example, if we called ageCalculator("Suzie", 1983, 2015);, 
-the return value should be as follows:
-
-return != console.log
-console.log is great for debugging and inspecting code by printing values to the terminal
-and is mostly used for this purpose. However, most functions are designed to return a
-computed value to the caller instead of printing it to the terminal.
-
-You should therefore carefully read for whether you need to return or log in your function.
-
-"Suzie is 32 years old."
-
-Tips:
-You may paste the following code after your function definition to help you verify it 
-works correctly.
+//use to verify if it works properly
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
